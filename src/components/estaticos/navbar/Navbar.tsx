67a944950f1adc15fddfa114ea1b4db8f67b9c1b
Amanda <amandinha_soto@hotmail.com>
2022-02-15 15:44:50 -0300

    Organizando pastas

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Navbar (){
+    return(
+    <>
+      const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+    </>
+
+    )
+}
+
+export default Navbar;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Button, Box, Paper} from '@material-ui/core';
+import './Home.css';
+import { height } from "@mui/system";
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    );
+}
+
+export default Home;
